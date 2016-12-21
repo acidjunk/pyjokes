09c6adb14aee0d3c4da97c2a16dd03a4c864dd51
Rene Dohmen <acidjunk@gmail.com>
2016-12-21 14:34:14 +0100

    Added more NL jokes

--- pyjokes/jokes_nl.py
+++ pyjokes/jokes_nl.py
@@ -7,6 +7,9 @@ Jokes by R. Dohmen - Provided under CC
 neutral = [
     "Wat is de overeenkomst tussen PyJokes en Adobe Flash? iEr blijven updates komen maar ze worden niet beter.",
     "Wat is het verschil tussen een Lada en een Lada sport? Het tennisballetje op de trekhaak.",
+    "Wat is het toppunt van lef? Een spookrijder proberen in te halen.",
+    "Wat is het toppunt van grofheid? Een ouderavond in een weeshuis organiseren.",
+    "Wat is het toppunt van grof zijn? Iemand met 1 been een step geven."
 ]
 
 adult = [
@@ -14,7 +17,11 @@ adult = [
 ]
 
 belgium = [
-    "Hoeveel belgen heb je nodig om een lamp te vervangen? negen; een belg om de lamp vast te hoduen, de rest om de tafel te draaien.",
+    "Hoeveel belgen heb je nodig om een lamp te vervangen? Negen; een belg om de lamp vast te hoduen, de rest om de tafel te draaien.",
+    "Waarom loopt een belg maandag ochtend altijd naast zijn fiets? Dan zit het weekend erop.",
+    "Hoe testen ze in belgie luciefers? Tsjck, doet het. Tsjck doet het.",
+    "De Belgen gaan hun eerste raket lanceren. 'en?' vraagt een verslaggever, 'waar gaat de reis naar toe?' 'Awel', zegt de Belg, 'wij gaan naar de zon.' 'Naar de zon?'' vraagt de reporter, 'Maar daar is het toch veel te heet?' 'Wij zijn niet gek, hoor. We gaan natuurlijk 's nachts!'".
+    "Wat moet je doen als een Belg een handgranaat naar je toe gooit? Pinnetje eruit halen en teruggooien.",
 ]
 
 jokes_nl = {
