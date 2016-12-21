6b4528fb124cb32b106ebd05b87d3231d79393d5
Rene Dohmen <acidjunk@gmail.com>
2016-12-21 09:13:28 +0100

    Nl Jokes starting point

---
+++ pyjokes/jokes_nl.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+Jokes by R. Dohmen - Provided under CC
+"""
+
+neutral = [
+    "Wat is de overeenkomst tussen PyJokes en Adobe Flash? iEr blijven updates komen maar ze worden niet beter.",
+    "Wat is het verschil tussen een Lada en een Lada sport? Het tennisballetje op de trekhaak.",
+]
+
+adult = [
+    "Programmeren is net sex: 1 fout en je blijft het je hele leven ondersteunen.",
+]
+
+belgium = [
+    "Hoeveel belgen heb je nodig om een lamp te vervangen? negen; een belg om de lamp vast te hoduen, de rest om de tafel te draaien.",
+]
+
+jokes_nl = {
+    'neutral': neutral,
+    'adult': adult,
+    'belgium': belgium,
+    'all': neutral + adult + belgium,
+}
